92b274bd63c2d251def8eb59d44b55de8f5424b9
Vndrvs <99025350+Vndrvs@users.noreply.github.com>
2025-11-22 19:00:46 +0700

    network and detail activity commented

--- app/src/main/java/gde/bike/mobileapp/DetailActivity.kt
+++ app/src/main/java/gde/bike/mobileapp/DetailActivity.kt
@@ -5,7 +5,9 @@ import gde.bike.mobileapp.databinding.DetailScreenBinding
 
 class DetailActivity: androidx.appcompat.app.AppCompatActivity() {
 
+    // viewbinding a detail layouthoz
     lateinit var binding: DetailScreenBinding
+    // adatbázis instance, itt is szükséges a kontextus miatt
     lateinit var db : AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,21 +15,25 @@ class DetailActivity: androidx.appcompat.app.AppCompatActivity() {
         binding = DetailScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Room database létrehozása
         db = AppDatabase.buildDb(this)
 
+        // előző képernyőről kapott bicikli azonosító (kell default érték)
         val bikeId = intent.getLongExtra(EXTRA_ID, -1L)
-
+        // adott bicikli lekérése az adatbázisból
         val bike = db.bikeDao().getBikeById(bikeId)
 
+        // sikeres lekérés esetén kitöltjük a UI-t a releváns adatokkal
         binding.brand.text = bike?.brand
         binding.model.text = bike?.model
         binding.price.text = bike?.price.toString()
         binding.category.text = bike?.category
         binding.description.text = bike?.description
         binding.isAvailable.text = bike?.availability
-
     }
+    // companion object magyarázat: https://www.baeldung.com/kotlin/companion-object
     companion object {
+        // intent kulcs az az ID átadáshoz
         const val EXTRA_ID = "id"
     }
 }
--- app/src/main/java/gde/bike/mobileapp/Network.kt
+++ app/src/main/java/gde/bike/mobileapp/Network.kt
@@ -1,33 +1,38 @@
 package gde.bike.mobileapp
 
-import androidx.room.Database
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import androidx.room.RoomDatabase
 
 public class Network {
+    // Retrofit példány, csak első használatkor hozzuk létre
     val retrofit: Retrofit by lazy {
+        // Retrofit Builder dokumentáció: https://square.github.io/retrofit/2.x/retrofit/retrofit2/Retrofit.Builder.html
         Retrofit.Builder()
+            // innen húzzuk le a JSON-t
             .baseUrl("https://raw.githubusercontent.com/udemx/hr-resources/refs/heads/master/")
+            // HTTP kliens + interceptor, ami kiírja a hálózati forgalmat a log-ba
             .client(OkHttpClient.Builder()
+                // OKHTTP logging interceptor dokumentáció: https://square.github.io/okhttp/3.x/logging-interceptor/okhttp3/logging/HttpLoggingInterceptor.html
                 .addInterceptor(HttpLoggingInterceptor().apply {
+                    // 'Body szintű' logolás
                     setLevel(HttpLoggingInterceptor.Level.BODY)
                 }).build()
             )
+            // JSON-ból Kotlin object konvertálás Gson-nal
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
+    // Retrofit által létrehozott API interface
     val apiService: ApiService by lazy {
         retrofit.create(ApiService::class.java)
     }
 }
 interface ApiService {
+    // GET request a bicycles.json fájlra, ebből lista 'bike' objectekből
     @GET("bicycles.json")
     fun loadBicycles(): Call<List<Bike>>
 }
